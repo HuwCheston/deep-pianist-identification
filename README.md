e81b10cd3474e16db796ab25a0d1cd2f699b7aaf
Huw Cheston <hwc31@cam.ac.uk>
2024-09-10 12:41:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 🎹🎻🥁 Deep Pianist Identification from JTD 🎹🎻🥁
+
+Deep pianist identification using the ![Jazz Trio Database](https://github.com/HuwCheston/Jazz-Trio-Database/). May include some disentanglement, who knows?
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='deep_pianist_identification',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Deep Pianist Identification using the JTD',
+    author='Huw Cheston',
+    license='MIT',
+)
