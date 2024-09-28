479578f6fc50451626dc0916f71ecfe6906fd515
Huw Cheston <hwc31@cam.ac.uk>
2024-09-28 09:25:14 +0100

    Added optional masking to disentanglenet

--- deep_pianist_identification/encoders/disentangle.py
+++ deep_pianist_identification/encoders/disentangle.py
@@ -3,6 +3,7 @@
 
 """Working towards an interpretable performer identification model from a multi-dimensional piano roll representation"""
 
+import numpy as np
 import torch
 import torch.nn as nn
 
@@ -18,6 +19,7 @@ class Concept(nn.Module):
         super(Concept, self).__init__()
         # Convolutional layers output size: [64, 64, 128, 128, 256, 256, 512, 512]
         # Each layer goes: conv -> dropout -> relu -> batchnorm -> avgpool
+        # TODO: consider whether to pool only every two layers as in Kong et al. model
         self.layer1 = ConvLayer(1, 64, has_pool=True)
         self.layer2 = ConvLayer(64, 128, has_pool=True)
         self.layer3 = ConvLayer(128, 256, has_pool=True)
@@ -42,25 +44,42 @@ class DisentangleNet(nn.Module):
             self,
             num_layers: int = 4,
             use_gru: bool = True,
-            num_heads: int = 2,
+            num_attention_heads: int = 2,
             use_masking: bool = False,
-            masking_prob: float = 0.5
+            masking_probability: float = 0.3,
+            max_masked_concepts: int = 3
     ):
         super(DisentangleNet, self).__init__()
         # Whether to randomly mask individual channels after processing
         self.use_masking = use_masking
-        self.masking_prob = masking_prob
+        self.masking_probability = masking_probability
+        self.max_masked_concepts = max_masked_concepts  # Max up to this number of concepts
+        self.dropper = nn.Dropout(p=1.0)  # Always deactivated during testing!
         # Layers for each individual musical concept
         self.melody_concept = Concept(num_layers, use_gru)
         self.harmony_concept = Concept(num_layers, use_gru)
         self.rhythm_concept = Concept(num_layers, use_gru)
         self.dynamics_concept = Concept(num_layers, use_gru)
         # Learn connections and pooling between concepts (i.e., channels)
-        self.self_attention = nn.MultiheadAttention(512, num_heads=num_heads, batch_first=True)
+        self.self_attention = nn.MultiheadAttention(512, num_heads=num_attention_heads, batch_first=True)
+        # TODO: consider whether max (or max + avg) pooling might work better here
         self.pooling = nn.AdaptiveAvgPool1d(1)
-        # Linear layers project on to final output
-        self.fc1 = LinearLayer(512, 128)
-        self.fc2 = LinearLayer(128, utils.N_CLASSES)
+        # Linear layers project on to final output: these have dropout added
+        self.fc1 = LinearLayer(512, 128, p=0.5)
+        self.fc2 = LinearLayer(128, utils.N_CLASSES, p=0.5)
+
+    def mask(self, embeddings: list[torch.tensor]) -> list[torch.tensor]:
+        # If we're applying masking
+        if np.random.uniform(0, 1) <= self.masking_probability:
+            # Choose between 1 and N concepts to mask
+            n_to_zero = np.random.randint(1, self.max_masked_concepts + 1)
+            # Get the required indexes from the list
+            idx_to_zero = np.random.choice(len(embeddings), n_to_zero, replace=False)
+            # Apply the masking
+            for idx in idx_to_zero:
+                # By using dropout, we ensure that we use all masks during testing
+                embeddings[idx] = self.dropper(embeddings[idx])
+        return embeddings
 
     def forward(self, x):
         # Split multi-dimensional piano roll into each concept
@@ -70,9 +89,12 @@ class DisentangleNet(nn.Module):
         harmony = self.harmony_concept(harmony)
         rhythm = self.rhythm_concept(rhythm)
         dynamics = self.dynamics_concept(dynamics)
-        # TODO: masking goes here
+        # Combine all embeddings into a single list and apply random masking if required
+        embeddings = [melody, harmony, rhythm, dynamics]
+        if self.use_masking:
+            embeddings = self.mask(embeddings)
         # Stack into a single tensor: (batch, channels, features)
-        x = torch.cat([melody, harmony, rhythm, dynamics], dim=1)
+        x = torch.cat(embeddings, dim=1)
         # Self-attention across channels
         x, _ = self.self_attention(x, x, x)
         # Pool across channels
@@ -91,18 +113,17 @@ if __name__ == '__main__':
     from torch.utils.data import DataLoader
 
     size = 1
-    model = DisentangleNet().to(utils.DEVICE)
     loader = DataLoader(
         MIDILoader(
             'train',
-            n_clips=2,
+            n_clips=10,
             multichannel=True,
         ),
         batch_size=size,
         shuffle=True,
         collate_fn=remove_bad_clips_from_batch
     )
-    model = DisentangleNet().to(utils.DEVICE)
+    model = DisentangleNet(use_masking=True, masking_probability=0.3).to(utils.DEVICE)
     print(utils.total_parameters(model))
     for feat, _ in loader:
         embeds = model(feat.to(utils.DEVICE))
