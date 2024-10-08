d7ce09dc17dbca387c67c012c4d38ab3b8730447
Huw Cheston <hwc31@cam.ac.uk>
2024-10-08 10:25:17 +0100

    Updates to random forest baselines

---
+++ deep_pianist_identification/rf_baselines/melody.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Random forest baseline applied to skylined MIDI melody files"""
+
+import os
+
+from loguru import logger
+from pretty_midi import PrettyMIDI
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+import deep_pianist_identification.rf_baselines.rf_utils as rf_utils
+from deep_pianist_identification import utils
+from deep_pianist_identification.extractors import MelodyExtractor, ExtractorError
+
+NGRAMS = [3, 4]
+VALID_NGRAMS = 20
+
+
+def extract_melody_ngrams(tpath: str, nclips: int, pianist: str, ngrams: list = None) -> tuple[dict, int]:
+    """For a given track path, number of clips, and pianist, extract all melody `ngrams` and return a dictionary"""
+    # Use the default ngram value, if not provided
+    if ngrams is None:
+        ngrams = NGRAMS
+    # Create a list of empty dictionaries, one per each value of n we wish to extract (e.g., 3-grams, 4-grams...)
+    ngram_res = [dict() for _ in range(len(ngrams))]
+    for idx in range(nclips):
+        # Load the clip in as a PrettyMIDI object
+        clip_path = os.path.join(utils.get_project_root(), tpath, f'clip_{str(idx).zfill(3)}.mid')
+        loaded = PrettyMIDI(clip_path)
+        # Try and create the piano roll, but skip if we end up with zero notes
+        try:
+            roll = MelodyExtractor(loaded, clip_start=0)
+        except ExtractorError as e:
+            logger.warning(f'Errored for {tpath}, clip {idx}, skipping! {e}')
+            continue
+        # Sort the notes by onset start time and calculate the intervals
+        melody = sorted(roll.output_midi.instruments[0].notes, key=lambda x: x.start)
+        intervals = [i2.pitch - i1.pitch for i1, i2 in zip(melody[0:], melody[1:])]
+        # Iterate through every ngram and corresponding results dictionary
+        for n, di in zip(ngrams, ngram_res):
+            # Extract the n-grams from the clip
+            grams = [intervals[i: i + n] for i in range(len(intervals) - n + 1)]
+            # Append each n-gram to the dictionary if not present already and increase the count by one
+            for ng in grams:
+                ng = str(ng)
+                if ng not in di.keys():
+                    di[ng] = 1
+                else:
+                    di[ng] += 1
+    # Collapse all the ngram dictionaries to a single dictionary and return alongside the ground truth label
+    return {k: v for d in ngram_res for k, v in d.items()}, pianist
+
+
+def rf_melody(n_iter: int, valid_ngrams_count: int, ngrams: list[int]) -> None:
+    """Create and optimize random forest melody classifier using provided command line arguemnts"""
+    logger.info("Creating baseline random forest classifier using melody data!")
+    # Get clips from both datasets
+    logger.info("Getting tracks from all data splits...")
+    train_clips = list(rf_utils.get_split_clips("train"))
+    test_clips = list(rf_utils.get_split_clips("test"))
+    logger.info(f"... found {len(train_clips)} training tracks and {len(test_clips)} testing tracks!")
+    # Get n-grams from both datasets
+    logger.info("Extracting n-grams from training tracks..")
+    temp_x, train_y = rf_utils.extract_ngrams_from_clips(train_clips, extract_melody_ngrams, ngrams)
+    logger.info("Extracting n-grams from testing tracks...")
+    test_x, test_y = rf_utils.extract_ngrams_from_clips(test_clips, extract_melody_ngrams, ngrams)
+    # Get those n-grams that appear in at least N tracks in the training dataset
+    logger.info(f"Extracting n-grams which appear in at least {valid_ngrams_count} training tracks...")
+    valid_ngs = rf_utils.get_valid_ngrams(temp_x, valid_count=valid_ngrams_count)
+    logger.info(f"... found {len(valid_ngs)} n-grams!")
+    # Subset both datasets to ensure we only keep valid ngrams
+    logger.info("Formatting features...")
+    train_x = rf_utils.drop_invalid_ngrams(temp_x, valid_ngs)
+    test_x = rf_utils.drop_invalid_ngrams(test_x, valid_ngs)
+    # Convert both training and testing features into numpy arrays with the same dimensionality
+    # TODO: we can add the validation split into this
+    train_x_arr, test_x_arr = rf_utils.format_features(train_x, test_x)
+    # Load the optimized parameter settings (or recreate them, if they don't exist)
+    logger.info(f"Beginning parameter optimization with {n_iter} iterations...")
+    csvpath = os.path.join(utils.get_project_root(), 'references/rf_baselines', 'melody.csv')
+    logger.info(f"... optimization results will be saved in {csvpath}")
+    optimized_params = rf_utils.optimize_classifier(train_x_arr, train_y, test_x_arr, test_y, csvpath, n_iter=n_iter)
+    # Create the optimized random forest model
+    clf_opt = RandomForestClassifier(**optimized_params, random_state=utils.SEED)
+    clf_opt.fit(train_x_arr, train_y)
+    # Get the optimized test accuracy
+    test_y_pred = clf_opt.predict(test_x_arr)
+    test_acc = accuracy_score(test_y, test_y_pred)
+    logger.info(f"Test accuracy for melody: {test_acc:.2f}")
+    # TODO: compute validation accuracy using optimized settings
+
+
+if __name__ == "__main__":
+    import argparse
+
+    utils.seed_everything(utils.SEED)
+    # Parsing arguments from the command line interface
+    parser = argparse.ArgumentParser(description='Create baseline random forest classifier for melody')
+    parser.add_argument(
+        "-i", "--n-iter", default=rf_utils.N_ITER, type=int, help="Number of optimization iterations"
+    )
+    parser.add_argument(
+        "-v", "--valid-ngrams-count", default=VALID_NGRAMS, type=int,
+        help="Valid n-grams appear in this many tracks"
+    )
+    parser.add_argument('-l', '--ngrams', nargs='+', type=int, help="Extract these n-grams", default=NGRAMS)
+    # Parse all arguments and create the forest
+    args = vars(parser.parse_args())
+    rf_melody(n_iter=args["n_iter"], ngrams=args["ngrams"], valid_ngrams_count=args["valid_ngrams_count"])
--- deep_pianist_identification/rf_baselines/rf_utils.py
+++ deep_pianist_identification/rf_baselines/rf_utils.py
@@ -7,9 +7,20 @@ import csv
 import json
 import os
 from ast import literal_eval
+from collections import defaultdict
+from pathlib import Path
 from tempfile import NamedTemporaryFile
+from typing import Callable, Iterable
 
+import numpy as np
+import pandas as pd
+from joblib import Parallel, delayed
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import ParameterSampler
 from tenacity import retry, retry_if_exception_type
+
+from deep_pianist_identification import utils
 
 # These are the parameters we'll sample from when optimizing
 OPTIMIZE_PARAMS = dict(
@@ -26,13 +37,13 @@ OPTIMIZE_PARAMS = dict(
     # Whether to sample data points with our without replacement
     bootstrap=[True, False],
 )
-N_ITER = 10000
-VALID_NGRAMS = 10
-NGRAMS = [3, 4]
+
+N_ITER = 10000  # default number of iterations for optimization process
+N_JOBS = -1  # number of parallel processing cpu cores. -1 means use all cores.
 
 
 @retry(retry=retry_if_exception_type(json.JSONDecodeError))
-def safe_load_csv(csv_path: str) -> dict:
+def threadsafe_load_csv(csv_path: str) -> dict:
     """Simple wrapper around `csv.load` that catches errors when working on the same file in multiple threads"""
 
     def eval_(i):
@@ -42,20 +53,23 @@ def safe_load_csv(csv_path: str) -> dict:
             return str(i)
 
     with open(csv_path, "r+") as in_file:
-        return [{k: eval_(v) for k, v in row.items()} for row in csv.DictReader(in_file, skipinitialspace=True)]
+        return [{k: eval_(v) for k, v in dict(row).items()} for row in csv.DictReader(in_file, skipinitialspace=True)]
 
 
-def safe_save_csv(obj, fdir: str, fpath: str) -> None:
+def threadsafe_save_csv(obj: list | dict, fpath: str) -> None:
     """Simple wrapper around csv.DictWriter with protections to assist in multithreaded access"""
+    # Get the base directory and filename from the provided path
+    _path = Path(fpath)
+    fdir, fpath = str(_path.parent), str(_path.name)
     # If we have an existing file with the same name, load it in and extend it with our new data
     try:
-        existing_file = safe_load_csv(os.path.join(fdir, fpath))
+        existing_file = threadsafe_load_csv(os.path.join(fdir, fpath))
     except FileNotFoundError:
         pass
     else:
         if isinstance(obj, dict):
             obj = [obj]
-        obj = existing_file + obj
+        obj: list = existing_file + obj
 
     # Create a new temporary file, in append mode
     temp_file = NamedTemporaryFile(mode='a', newline='', dir=fdir, delete=False, suffix='.csv')
@@ -81,3 +95,104 @@ def safe_save_csv(obj, fdir: str, fpath: str) -> None:
         os.replace(temp_file.name, os.path.join(fdir, fpath))
 
     replacer()
+
+
+def get_split_clips(split_name: str) -> tuple[str, int, str]:
+    """For a given CSV, yields tuples of track path, N track clips, track target"""
+    # Quick check to make sure the split name is valid
+    assert split_name in ["test", "train", "validation"], "`split_name` must be one of 'test', 'train', or 'validation"
+    # Load the dataframe
+    split_path = os.path.join(utils.get_project_root(), 'references/data_splits', f'{split_name}_split.csv')
+    test_clips = pd.read_csv(split_path, index_col=0, delimiter=',')
+    # Iterate through rows (= tracks)
+    for idx, track in test_clips.iterrows():
+        # Get the location of the clip folder
+        track_path = os.path.join(utils.get_project_root(), 'data/clips', track['track'])
+        # Count the number of clips for this track
+        track_clips = len(os.listdir(track_path))
+        # Return a tuple for each track
+        yield track_path, track_clips, track['pianist']
+
+
+def extract_ngrams_from_clips(split_clips: Iterable, extract_func: Callable, *args) -> tuple[list[dict], list[int]]:
+    """For clips from a given split, applies an n-gram `extract_func` in parallel"""
+    # Iterate over each track and apply our extraction function
+    with Parallel(n_jobs=N_JOBS, verbose=5) as par:
+        res = par(delayed(extract_func)(p, n, pi, *args) for p, n, pi in list(split_clips))
+    # Returns a tuple of ngrams (dict) and targets (int) for every *clip* in the dataset
+    return zip(*res)
+
+
+def get_valid_ngrams(list_of_ngram_dicts: list[dict], valid_count: int) -> list[str]:
+    """From a list of n-gram dictionaries (one dictionary per track), get n-grams that appear in at least N tracks"""
+    ngram_counts = defaultdict(int)
+    # Iterate through each dictionary (= one track)
+    for d in list_of_ngram_dicts:
+        # Iterate through each key (= one n-gram in the track)
+        for key in d:
+            # Increase the global count for this n-gram
+            ngram_counts[key] += 1
+    # Return a list of n-grams that appear in at least N tracks
+    return [key for key, count in ngram_counts.items() if count >= valid_count]
+
+
+def drop_invalid_ngrams(list_of_ngram_dicts: list[dict], valid_ngrams: list[str]) -> list[dict]:
+    """From a list of n-gram dictionaries (one dictionary per track), remove n-grams that do not appear in N tracks"""
+    # Define the removal function
+    rm = lambda feat: {k: v for k, v in feat.items() if k in valid_ngrams}
+    # In parallel, remove non-valid n-grams from all dictionaries
+    with Parallel(n_jobs=N_JOBS, verbose=5) as par:
+        return par(delayed(rm)(f) for f in list_of_ngram_dicts)
+
+
+def format_features(*features: list[dict]) -> tuple:
+    """For an arbitrary number of ngram dictionaries, format these as NumPy arrays with the same number of columns"""
+    fmt = []
+    for split_count, feature in enumerate(features):
+        # Convert features to a dataframe
+        df = pd.DataFrame(feature)
+        # Set ID column
+        df['split'] = str(split_count)
+        fmt.append(df)
+    # Combine all dataframes row-wise, which ensures the correct number of columns in all, and fill NaNs with 0
+    conc = pd.concat(fmt, axis=0).fillna(0)
+    # Return a tuple of numpy arrays, one for each split initially provided
+    return (conc[conc['split'] == str(sp)].drop(columns='split').to_numpy() for sp in range(len(features)))
+
+
+def optimize_classifier(
+        train_features: np.ndarray, train_targets: list, test_features: np.ndarray, test_targets: np.ndarray,
+        out: str, classifier: Callable = RandomForestClassifier, n_iter: int = N_ITER, use_cache: bool = True
+) -> dict:
+    """With given training and test features/targets, optimize random forest for test accuracy and save CSV to `out`"""
+
+    def _step(iteration: int, parameters: dict) -> dict:
+        """Inner optimization function"""
+        # Try and load in the CSV file for this particular task
+        if use_cache:
+            try:
+                cached_results = threadsafe_load_csv(out)
+                # If we have it, just return the cached results for this particular iteration
+                return [o for o in cached_results if o['iteration'] == iteration][0]
+            # Don't worry if we haven't got the CSV file or results for this interation yet
+            except (FileNotFoundError, IndexError):
+                pass
+        # Create the forest model with the given parameter settings
+        forest = classifier(**parameters, random_state=utils.SEED)
+        # Fit the model to the training data and get the test data accuracy
+        forest.fit(train_features, train_targets)
+        acc = accuracy_score(test_targets, forest.predict(test_features))
+        # Create the results dictionary and save
+        results_dict = {'accuracy': acc, 'iteration': iteration, **parameters}
+        threadsafe_save_csv(results_dict, out)
+        # Return the results for this optimization step
+        return results_dict
+
+    # Create the parameter sampling instance with the required parameters, number of iterations, and random state
+    sampler = ParameterSampler(OPTIMIZE_PARAMS, n_iter=n_iter, random_state=utils.SEED)
+    # Use lazy parallelization to create the forest and fit to the data
+    with Parallel(n_jobs=N_JOBS, verbose=5) as p:
+        fit = p(delayed(_step)(num, params) for num, params in enumerate(sampler))
+    # Get the parameter combination that yielded the best test set accuracy
+    best_params = max(fit, key=lambda x: x["accuracy"])
+    return {k: v for k, v in best_params.items() if k not in ["accuracy", "iteration"]}
