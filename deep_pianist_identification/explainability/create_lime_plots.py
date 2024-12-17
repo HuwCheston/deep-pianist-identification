5a56da2af15511265b49cb7d456a37029928d2f3
Huw Cheston <hwc31@cam.ac.uk>
2024-12-17 08:05:36 +0000

    Aesthetic adjustments for LIME plots

--- deep_pianist_identification/explainability/create_lime_plots.py
+++ deep_pianist_identification/explainability/create_lime_plots.py
@@ -3,8 +3,10 @@
 
 """Creates masked piano roll plots for held-out data using trained resnet and LIME"""
 
+import json
 import os
 from copy import deepcopy
+from datetime import timedelta
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,6 +15,7 @@ import torch
 import yaml
 from lime import lime_image
 from loguru import logger
+from pretty_midi import note_number_to_name
 from skimage.color import rgb2gray
 from skimage.segmentation import mark_boundaries
 from skimage.util import invert
@@ -23,8 +26,8 @@ from deep_pianist_identification.training import TrainModule, DEFAULT_CONFIG
 
 MODEL_NAME = "baselines/resnet50-jtd+pijama-augment"
 
-NUM_SAMPLES = 1000
-NUM_FEATURES = 10
+N_SAMPLES = 100
+N_FEATURES = 5
 
 
 class HeatmapLIMEPianoRoll(plotting.BasePlot):
@@ -34,7 +37,6 @@ class HeatmapLIMEPianoRoll(plotting.BasePlot):
     EXPLAIN_KWS = dict(
         top_labels=5,
         hide_color=0,
-        num_samples=NUM_SAMPLES,
         random_seed=utils.SEED,
         distance_metric='cosine'
     )
@@ -46,16 +48,22 @@ class HeatmapLIMEPianoRoll(plotting.BasePlot):
     def __init__(
             self,
             piano_roll: np.array,
-            classifier,
+            clip_path: str,
+            classifier: torch.nn.Module,
             pianist_name: str,
-            n_features: int = 5
+            n_features: int = N_FEATURES,
+            n_samples: int = N_SAMPLES
     ):
         super().__init__()
+        self.clip_path = clip_path
+        # Matplotlib figure and axis
+        self.clip_start, self.clip_end = self.parse_clip_start_stop(clip_path)
         # Don't modify the underlying piano roll
         self.piano_roll = deepcopy(piano_roll)
         self.classifier = classifier
         self.pianist_name = pianist_name
         self.n_features = n_features
+        self.n_samples = n_samples
         self.fig, self.ax = plt.subplots(
             1, 1, figsize=(plotting.WIDTH, plotting.WIDTH // 3)
         )
@@ -78,18 +86,41 @@ class HeatmapLIMEPianoRoll(plotting.BasePlot):
         softmaxed = torch.nn.functional.softmax(logits, dim=1)
         return softmaxed.cpu().numpy()
 
+    @staticmethod
+    def seconds_to_timedelta(seconds: int):
+        # Format in %M:%S format
+        td = timedelta(seconds=seconds)
+        return str(td)[2:]
+
+    @staticmethod
+    def parse_clip_start_stop(clip_path: str):
+        """Parse clip start and stop time from clip number using filepath"""
+        clip_num = int(clip_path.split('_')[-1])
+        clip_start = utils.CLIP_LENGTH * clip_num
+        clip_end = clip_start + utils.CLIP_LENGTH
+        return clip_start, clip_end
+
+    def parse_human_readable_trackname(self):
+        clip_start_fmt = self.seconds_to_timedelta(self.clip_start)
+        clip_end_fmt = self.seconds_to_timedelta(self.clip_end)
+        json_path = os.path.join(utils.get_project_root(), 'data/raw', self.clip_path.split('_')[0], 'metadata.json')
+        loaded = json.load(open(json_path, 'r'))
+        return (f'{loaded["bandleader"]} — "{loaded["track_name"]}" ({clip_start_fmt}—{clip_end_fmt}) '
+                f'\nfrom "{loaded["album_name"]}", {loaded["recording_year"]}')
+
     def get_explanation(self):
         explanation = self.EXPLAINED.explain_instance(
             self.piano_roll,
             self.classification_function,
             num_features=self.n_features,
+            num_samples=self.n_samples,
             **self.EXPLAIN_KWS
         )
         temp, mask = explanation.get_image_and_mask(
             explanation.top_labels[0],
             **self.IMAGE_MASK_KWS
         )
-        return invert(mark_boundaries(temp, mask))
+        return invert(mark_boundaries(temp / 255., mask))
 
     def create_plot(self):
         self.masked_image = self.get_explanation()
@@ -125,13 +156,13 @@ class HeatmapLIMEPianoRoll(plotting.BasePlot):
         self.ax.grid(**plotting.GRID_KWS)
         # Set axes limits correctly
         self.ax.set(
-            title=f'Pianist: {self.pianist_name}',
-            xticks=list(range(0, utils.CLIP_LENGTH * utils.FPS + 500, 500)),
+            title=self.parse_human_readable_trackname(),
+            xticks=range(0, (utils.CLIP_LENGTH * utils.FPS + 1), utils.FPS * 5),
+            xticklabels=[self.seconds_to_timedelta(i) for i in range(self.clip_start, self.clip_end + 1, 5)],
             yticks=range(0, utils.PIANO_KEYS, utils.OCTAVE),
-            xticklabels=list(range(0, utils.CLIP_LENGTH + 5, 5)),
-            yticklabels=range(0, utils.PIANO_KEYS, utils.OCTAVE),
+            yticklabels=[note_number_to_name(i + utils.MIDI_OFFSET) for i in range(0, utils.PIANO_KEYS, 12)],
             xlabel='Time (seconds)',
-            ylabel='MIDI note number'
+            ylabel='Note'
         )
         self.ax.tick_params(axis='x', rotation=0)
         # self.ax.invert_yaxis()
@@ -141,7 +172,7 @@ class HeatmapLIMEPianoRoll(plotting.BasePlot):
 
     def _format_fig(self):
         self.fig.subplots_adjust(
-            right=1.075, left=0.05, top=0.9, bottom=0.15
+            right=1.075, left=0.05, top=0.875, bottom=0.15
         )
 
     def save_fig(self, outpath: str):
@@ -153,7 +184,7 @@ class HeatmapLIMEPianoRoll(plotting.BasePlot):
         if not os.path.isdir(dirpath):
             os.makedirs(dirpath)
         self.fig.savefig(
-            os.path.join(dirpath, f'{outpath}.png'),
+            os.path.join(dirpath, f'{outpath.split("/")[1]}.png'),
             **plotting.SAVE_KWS
         )
         plt.close('all')
@@ -180,7 +211,8 @@ def create_plots(dataset, class_mapping, model):
             continue
         roll, _, clip_path = ret
         clip_path = f'{clip_path}_clip_{str(clip_idx).zfill(3)}'
-        hm = HeatmapLIMEPianoRoll(roll[0], model, target_pianist, n_features=NUM_FEATURES)
+        logger.info(f'Processing {clip_path}')
+        hm = HeatmapLIMEPianoRoll(roll[0], clip_path, model, target_pianist)
         hm.create_plot()
         hm.save_fig(clip_path)
 
@@ -216,4 +248,5 @@ def main():
 
 
 if __name__ == "__main__":
+    utils.seed_everything(utils.SEED)
     main()
