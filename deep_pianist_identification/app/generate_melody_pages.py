6340f9236281086465e110c97c1806ac603204c8
Huw Cheston <hwc31@cam.ac.uk>
2025-01-23 18:08:59 +0000

    More work on melody pages

--- deep_pianist_identification/app/generate_melody_pages.py
+++ deep_pianist_identification/app/generate_melody_pages.py
@@ -6,7 +6,10 @@
 import json
 import os
 
+import numpy as np
 from PIL import Image
+from bs4 import BeautifulSoup
+from loguru import logger
 from pretty_midi import PrettyMIDI, Note, Instrument
 from tqdm import tqdm
 
@@ -19,7 +22,7 @@ DATASET = "20class_80min"
 INPUT_DIR = os.path.join(utils.get_project_root(), 'reports/figures/whitebox/lr_weights')
 PIANIST_MAPPING = {v: k for k, v in utils.get_class_mapping(DATASET).items()}
 
-N_EXAMPLES_PER_FEATURE = 5  # for every combination of feature and pianist, we'll generate this many examples
+MAX_EXAMPLES_PER_CLIP = 3  # we'll only get up to this many examples from the same clip
 
 
 def parse_input(input_fpath: str, extension: str = None) -> str:
@@ -69,27 +72,6 @@ def get_melody(clip_path: str) -> PrettyMIDI | None:
         return None
 
 
-def get_feature_matches(pianist_name: str, pianist_features: dict, pianist_tracks: list) -> dict:
-    """Get timestamps from a pianist's tracks that match features they are associated with"""
-    # We'll store results in here
-    pianist_matches = {str(feat): {} for feat in pianist_features}
-    # Iterate through all tracks by the pianist
-    for track_fpath, n_clips, _ in tqdm(pianist_tracks, desc=f'Getting matches for {pianist_name}'):
-        # Iterate over all the clips associated with this track
-        for clip_idx in range(n_clips):
-            # Load in the clip and extract melody using the skyline
-            clip_fpath = os.path.join(track_fpath, f'clip_{str(clip_idx).zfill(3)}.mid')
-            mel = get_melody(clip_fpath)
-            if mel is None:
-                continue
-            # Iterate over all features associated with this pianist
-            for feature in pianist_features:
-                # Get all timestamps in the clip that match with this feature
-                feature_match = extract_feature_timestamps(mel, feature)
-                pianist_matches[str(feature)][clip_fpath] = feature_match
-    return pianist_matches
-
-
 def note_list_to_pretty_midi(note_list: list[Note]) -> PrettyMIDI:
     """Convert a python list of pretty_midi.Note objects to a pretty_midi.PrettyMIDI instance"""
     new_midi = PrettyMIDI()
@@ -97,35 +79,6 @@ def note_list_to_pretty_midi(note_list: list[Note]) -> PrettyMIDI:
     new_instrument.notes = note_list
     new_midi.instruments = [new_instrument]
     return new_midi
-
-
-def edit_midi_by_timestamps(
-        midi: PrettyMIDI,
-        timestamps: list[tuple],
-        match_velocity: int = utils.MAX_VELOCITY,
-        non_match_velocity: int = utils.MAX_VELOCITY // 2
-) -> PrettyMIDI:
-    """With a given midi and list of (start, stop) windows, adjust velocity of notes contained within a window"""
-    notes_in_windows = []
-    # Iterate over all timestamps
-    for match_start, match_end in timestamps:
-        # Iterate over all notes in the midi
-        for note in midi.instruments[0].notes:
-            # If this note is contained within the matching window
-            if match_start <= note.start <= match_end:
-                # Increase its velocity and add to our list
-                note.velocity = match_velocity
-                notes_in_windows.append(note)
-    notes_not_in_windows = []
-    # Iterate over all notes again
-    for note in midi.instruments[0].notes:
-        # If we didn't match this note with a window
-        if note.start not in [n.start for n in notes_in_windows]:
-            # Decrease its velocity and add to the other list
-            note.velocity = non_match_velocity
-            notes_not_in_windows.append(note)
-    # Convert both lists of notes back to a pretty MIDI object
-    return note_list_to_pretty_midi(notes_in_windows + notes_not_in_windows)
 
 
 def generate_clip_metadata(clip_name: str) -> dict:
@@ -150,38 +103,67 @@ def generate_clip_metadata(clip_name: str) -> dict:
 def truncate_midi_between_timestamps(midi: PrettyMIDI, start: float, stop: float, pad: float = 2.) -> PrettyMIDI:
     # Get notes from the MIDI object that are contained within the span of our window
     truncated = [n for n in midi.instruments[0].notes if (start - pad) <= n.start <= (stop + pad)]
-    return note_list_to_pretty_midi(truncated)
+    # Edit note start and end times
+    try:
+        first_start = min(truncated, key=lambda x: x.start).start
+    except ValueError:  # why is this happening occasionally?
+        logger.warning(f"... failed with start {start}, stop {stop}")
+        return None
+    first_end = min(truncated, key=lambda x: x.end).end
+    offset = [
+        Note(start=n.start - first_start, end=n.end - first_end, pitch=n.pitch, velocity=n.velocity)
+        for n in truncated
+    ]
+    # Convert the list to a pretty MIDI object
+    return note_list_to_pretty_midi(offset)
 
 
-def generate_midi_from_matching_tracks(pianist_matches: dict, pianist_name: str) -> dict:
-    """Generates MIDI for tracks that match each feature a pianist is strongly associated with"""
+def get_feature_matches(pianist_name: str, pianist_features: dict, pianist_tracks: list) -> dict:
+    """Get timestamps from a pianist's tracks that match features they are associated with"""
+    # We'll store results in here
     new_dict = {}
-    # Iterate through features and timestamps
-    for feature_idx, (feature, matches) in enumerate(pianist_matches.items()):
-        new_dict[feature] = []
-        # Now, we can sort each track by the number of times a feature appears
-        sorted_matches_for_feature = sorted(matches.items(), key=lambda item: len(item[1]), reverse=True)
-        match_idx = 0
-        # Iterate over these tracks and the timestamps that match the feature
-        for matched_fname, matched_timestamps in sorted_matches_for_feature:
-            # Skip over if this track doesn't contain any appearances of the current feature
-            if len(matched_timestamps) == 0:
-                continue
-            # Get metadata for this clip
-            clip_metadata = generate_clip_metadata(matched_fname)
-            # Load the track in as a PrettyMIDI object
-            loaded_match = PrettyMIDI(matched_fname)
-            for match_start, match_end in matched_timestamps:
-                truncated_midi = truncate_midi_between_timestamps(loaded_match, match_start, match_end)
-                # Create the filepath for this object
-                fname = f"{pianist_name.lower().replace(' ', '_')}_{str(feature_idx).zfill(3)}_{str(match_idx).zfill(3)}"
-                fpath = os.path.join(utils.get_project_root(), f'app/assets/midi/melody_examples/{fname}.mid')
-                # Dump the MIDI object at the filepath
-                truncated_midi.write(fpath)
-                # Add information to dictionary
-                clip_metadata["asset_path"] = fpath
-                new_dict[feature].append(clip_metadata)
-                match_idx += 1  # increment counter by one
+    # Iterate over all features that this pianist is associated with
+    for feature_idx, feature in enumerate(pianist_features, 1):
+        count = 0  # used to track number of times we've seen this feature
+        new_dict[str(feature)] = []
+        # Iterate through all tracks by the pianist
+        for track_fpath, n_clips, _ in tqdm(
+                pianist_tracks, desc=f'Getting matches for {pianist_name}, feature {feature_idx}'
+        ):
+            # Iterate over all the clips associated with this track
+            for clip_idx in range(n_clips):
+                # Load in the clip and extract melody using the skyline
+                clip_fpath = os.path.join(track_fpath, f'clip_{str(clip_idx).zfill(3)}.mid')
+                # If we can't extract the melody for whatever reason, skip over this clip
+                mel = get_melody(clip_fpath)
+                if mel is None:
+                    continue
+                matches = extract_feature_timestamps(mel, feature)
+                if len(matches) == 0:
+                    continue
+                full_midi = PrettyMIDI(clip_fpath)
+                # Randomly shuffle the matches so we don't just get loads of appearances of the same pattern in a row
+                np.random.shuffle(matches)
+                n_clip_examples_seen = 0
+                for match_start, match_end in matches:
+                    clip_metadata = generate_clip_metadata(clip_fpath)
+                    truncated = truncate_midi_between_timestamps(full_midi, match_start, match_end)
+                    if truncated is None:
+                        continue
+                    # Create the filepath for this object
+                    fname = f"{pianist_name.lower().replace(' ', '_')}_{str(feature_idx).zfill(3)}_{str(count).zfill(3)}"
+                    fpath = os.path.join(utils.get_project_root(), f'app/assets/midi/melody_examples/{fname}.mid')
+                    # Dump the MIDI object at the filepath
+                    truncated.write(fpath)
+                    # Add information to dictionary
+                    clip_metadata["track_name"] = f'{count + 1}. {clip_metadata["track_name"]}'
+                    clip_metadata["asset_path"] = fname
+                    new_dict[str(feature)].append(clip_metadata)
+                    count += 1  # increment counter for number of times we've seen this feature in total
+                    n_clip_examples_seen += 1  # increment counter for times we've seen this feature this clip
+                    # Stop getting examples from this clip once we've got enough
+                    if n_clip_examples_seen > MAX_EXAMPLES_PER_CLIP:
+                        break
     return new_dict
 
 
@@ -218,11 +200,48 @@ def save_melody_feature_outputs(melody_feature: list[int], feature_rank: int, pi
     # If we haven't already created the image, go ahead and do this
     if not os.path.isfile(img_path):
         feature_image = m21_utils.score_to_image_array(feature_m21, dpi=800)
+        # Get the original dimensions
+        height, width, _ = feature_image.shape
         # Convert image array to pillow and save in desired location
-        Image.fromarray(feature_image).save(img_path)
+        Image.fromarray(feature_image).crop((0, 50, width, height - 50)).save(img_path)
     # If we haven't already created the MIDI, do this as well
     if not os.path.isfile(midi_path):
         feature_m21.write('midi', midi_path)
+
+
+def generate_html(pianist_metadata, pianist_name, tmp_pianist: str = "Abdullah Ibrahim"):
+    """Creates HTML by updating template and stores in app/pages/melody/<pianist>.html"""
+    # The concepts this pianist is associated with
+    features = list(pianist_metadata.keys())
+    # Load in the template and get the concept gallery, the element we need to modify
+    template = os.path.join(utils.get_project_root(), 'app/pages/templates/melody.html')
+    soup = BeautifulSoup(open(template, 'r'), features="lxml")
+    gallery = soup.find_all('div', {"class": "concept-gallery"})[0]
+    pianist_name_lower = pianist_name.lower().replace(' ', '_')
+    # Iterating through each "row" in the concept gallery
+    for feature_num, (tag, button, anchor, feat) in enumerate(zip(
+            gallery.find_all('strong'),
+            gallery.find_all('button'),
+            gallery.find_all('a'),
+            features
+    )):
+        # Updating all the headings with the correct concept name
+        tag.string.replace_with(feat)
+        # Updating all the buttons to use the correct argument
+        button['onclick'] = f"playExampleMidi({feature_num}, '{pianist_name}')"
+        # Updating all the anchor tags
+        anchor['alt'] = feat
+        feat_fmt = feat.replace("'", r"\'").replace('"', r'\"')
+        anchor['onclick'] = f"""popSelect('{feat_fmt}', '{pianist_name}')"""
+        # We can also grab the image from each anchor tag
+        img = anchor.find('img')
+        img['alt'] = feat
+        img['src'] = f"../../assets/images/melody_features/{pianist_name_lower}_{str(feature_num).zfill(3)}.png"
+    # Create the output path that we'll save the HTML in
+    out_path = os.path.join(utils.get_project_root(), 'app/pages/melody', pianist_name_lower + '.html')
+    # Write HTML and replace any reference to our placeholder pianist (Abdullah Ibrahim, default) with the actual one
+    with open(out_path, "w", encoding='utf-8') as f:
+        f.write(str(soup).replace(tmp_pianist, pianist_name))
 
 
 def main():
@@ -239,9 +258,7 @@ def main():
         # Each clip is in the form (track name, number of clips, pianist idx)
         pianist_clips = [i for i in clips if i[2] == PIANIST_MAPPING[pianist_name]]
         # Get the tracks that match each feature associated with the pianist
-        pianist_matches = get_feature_matches(pianist_name, pianist_features, pianist_clips)
-        # Edit the MIDI files and generate the MIDI
-        pianist_metadata = generate_midi_from_matching_tracks(pianist_matches, pianist_name)
+        pianist_metadata = get_feature_matches(pianist_name, pianist_features, pianist_clips)
         # Dump the metadata
         metadata_save_path = os.path.join(
             utils.get_project_root(),
@@ -250,8 +267,7 @@ def main():
         )
         with open(metadata_save_path, 'w') as js_out:
             json.dump(pianist_metadata, js_out, indent=4, ensure_ascii=False)
-
-    # TODO: copy the template html and fill in required information then save inside app/assets/pages/melody
+        generate_html(pianist_metadata, pianist_name)
 
 
 if __name__ == "__main__":
